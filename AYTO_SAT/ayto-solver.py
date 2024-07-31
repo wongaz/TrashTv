f27b4602bccec4bc5e5c9ad46cfe894c8899129a
Alex <awong593@gmail.com>
2024-07-31 15:02:18 -0700

    model works

--- AYTO_SAT/ayto-solver.py
+++ AYTO_SAT/ayto-solver.py
@@ -18,7 +18,6 @@ model.TruthBoothTruePair = Set(within=model.Male * model.Female)
 model.Beams = Param(model.Week, within=NonNegativeIntegers)
 model.Assignment = Param(model.Male, model.Female, model.Week, default=0.0, within=Binary)
 
-
 model.PerfectMatch = Var(model.Male, model.Female, within=Binary)
 
 
@@ -31,11 +30,18 @@ def optimality_condition(model):
 model.optimization = Objective(rule=optimality_condition, sense=maximize)
 
 
-def only_one_rule(model, boys):
-    return sum(model.PerfectMatch[boys, g] for g in model.Female) <= 1
+def only_one_boys_rule(model, boy):
+    return sum(model.PerfectMatch[boy, g] for g in model.Female) <= 1
 
 
-model.limit_one_rule = Constraint(model.Male, rule=only_one_rule)
+model.limit_one_boys_rule = Constraint(model.Male, rule=only_one_boys_rule)
+
+
+def only_one_girl_rule(model, girl):
+    return sum(model.PerfectMatch[b, girl] for b in model.Male) <= 1
+
+
+model.limit_one_girl_rule = Constraint(model.Female, rule=only_one_girl_rule)
 
 
 def derive_match_rule(model, week):
@@ -48,7 +54,7 @@ model.match_rule = Constraint(model.Week, rule=derive_match_rule)
 
 def truth_booth_false_rule(model, b, g):
     if (b, g) in model.TruthBoothFalsePair:
-        return 1 * model.PerfectMatch[b, g] <= 0
+        return model.PerfectMatch[b, g] <= 0
     else:
         return Constraint.Skip
 
@@ -65,7 +71,7 @@ def truth_booth_true_rule(model, b, g):
 
 model.successful_truth_booth = Constraint(model.Male, model.Female, rule=truth_booth_true_rule)
 
-instance = model.create_instance("Season/1/S1_EP4.dat")
+instance = model.create_instance("Season/1/S1_EP8.dat")
 
 # Indicate which solver to use
 Opt = SolverFactory("appsi_highs")
@@ -77,3 +83,21 @@ instance.solutions.load_from(Soln)
 # Print the output
 print("Termination Condition was " + str(Soln.Solver.Termination_condition))
 display(instance)
+
+final_pairings = []
+for v in instance.component_objects(Var, active=True):
+    # print ("Variable",v)
+    varobject = getattr(instance, str(v))
+    for index in varobject:
+        if varobject[index].value == 1:
+            final_pairings += [index]
+print("\n\n\n")
+
+
+def prCyan(skk): print("\033[96m {}\033[00m".format(skk))
+
+
+prCyan("Final Pairings")
+
+for k in final_pairings:
+    print(k)
